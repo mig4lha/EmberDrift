7699bb63b03b7526232a04c8fa1fa5118dc33bab
JOAO SILVA CARVALHO <150021934+Jucasix@users.noreply.github.com>
2026-06-03 11:42:35 +0100

    Delete Scripts directory

--- Scripts/Core/SaveData.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct SaveData: Codable {
-    var ash: Int = 0
-    var emberTree: EmberTreeState = .init()
-    var totalRuns: Int = 0
-    var bestTimeSurvivedSeconds: Int = 0
-}
-
